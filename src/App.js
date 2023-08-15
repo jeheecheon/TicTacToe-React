5980ff1fe48117af5429b6a84e1c1300afed4c0d
Jehee Cheon <jeheecheon@gmail.com>
2023-08-16 03:19:01 +0900

    README 수정 및 보드판 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import React from 'react'; // Import the React object
+import React from "react"; // Import the React object
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked! ".concat({value}.value));
+  }
+  return <button onClick={handleClick} className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </React.Fragment>
+  );
 }
